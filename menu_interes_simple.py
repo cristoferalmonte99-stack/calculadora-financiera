88dd2bf96a355a23fb712346cc677b3bc018b3f7
Cristofer <cristoferalmonte99@gmail.com>
2026-04-08 00:33:33 -0400

    Primer avance de interes simple

--- menu_interes_simple.py
+++ menu_interes_simple.py
@@ -1,3 +1,11 @@
+import logica_interes_simple
+ 
+def pedir_datos_interes():
+    capital = float(input("Ingrese el capital: "))
+    tasa = float(input("Ingrese la tasa (%): "))
+    tiempo = float(input("Ingrese el tiempo: "))
+    return capital, tasa, tiempo
+
 def menu_interes_simple():
     while(True):
         try:
@@ -11,7 +19,18 @@ def menu_interes_simple():
                   """)
             
             try:
-                opcion = intinput
+                opcion = int(input("Ingrese una opcion: "))
+            except ValueError as e:
+                print("Error: Solo se aceptan caracteres numericos ")
+                continue
+            
+            if opcion == 0: 
+                break
+            elif opcion == 1:
+                capital, tasa, tiempo = pedir_datos_interes()
+                resultado = logica_interes_simple.calcular_interes_simple(capital, tasa, tiempo)
+                print(f"El interes generado es: {resultado}")
+                
         
         except Exception as e:
             print(f"Error: {e}")
